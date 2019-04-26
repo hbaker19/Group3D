7c72244b7fd28d587477cd346371314c6e92eb6a
Colby Johnson <20.colby.johnson@ksd.org>
2019-04-26 10:29:16 -0700

    ColbyFixing

---
+++ Assets/Handmade stuff/ColbyUserController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ColbyUserController : MonoBehaviour
+{
+    private ColbyMovement m_Character;
+    private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+    private Vector3 m_CamForward;             // The current forward direction of the camera
+    private Vector3 m_Move;
+    private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Camera.main != null)
+        {
+            m_Cam = Camera.main.transform;
+        }
+        else
+        {
+            Debug.LogWarning(
+                "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+            // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+        }
+
+        m_Character = GetComponent<ColbyMovement>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_Jump)
+        {
+            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+        if (m_Cam != null)
+        {
+            // calculate camera relative direction to move:
+            m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+            m_Move = v * m_CamForward + h * m_Cam.right;
+        }
+        else
+        {
+            // we use world-relative directions in the case of no main camera
+            m_Move = v * Vector3.forward + h * Vector3.right;
+        }
+#if !MOBILE_INPUT
+        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+#endif
+
+        // pass all parameters to the character control script
+        m_Character.Move(m_Move,  m_Jump);
+        m_Jump = false;
+    }
+
+
+}
---
+++ Assets/Handmade stuff/Hero(Colby)/ColbyMovement.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Animator))]
+public class ColbyMovement : MonoBehaviour
+{
+
+    [SerializeField] float m_JumpPower = 12f;
+    [Range(1f, 4f)] [SerializeField] float m_GravityMultiplier = 2f;
+    [SerializeField] float m_MoveSpeedMultiplier = 1f;
+    [SerializeField] float m_AnimSpeedMultiplier = 1f;
+    [SerializeField] float m_GroundCheckDistance = 0.1f;
+    Rigidbody m_Rigidbody;
+    Animator m_Animator;
+    //-----------
+   public bool Grounded;
+    float m_OrigGroundCheckDistance;
+    float m_ForwardAmount;
+    Vector3 m_GroundNormal;
+    float m_CapsuleHeight;
+    Vector3 m_CapsuleCenter;
+    CapsuleCollider m_Capsule;
+    //-----------
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Capsule = GetComponent<CapsuleCollider>();
+        m_CapsuleHeight = m_Capsule.height;
+        m_CapsuleCenter = m_Capsule.center;
+
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        m_OrigGroundCheckDistance = m_GroundCheckDistance;
+    }
+
+
+    public void Move(Vector3 move, bool jump)
+    {
+
+        // convert the world relative moveInput vector into a local-relative
+        // turn amount and forward amount required to head in the desired
+        // direction.
+        if (move.magnitude > 1f) move.Normalize();
+        move = transform.InverseTransformDirection(move);
+        CheckGroundStatus();
+        move = Vector3.ProjectOnPlane(move, m_GroundNormal);
+        m_ForwardAmount = move.z;
+
+        // control and velocity handling is different when grounded and airborne:
+        if (Grounded)
+        {
+            HandleGroundedMovement(jump);
+        }
+        else
+        {
+            HandleAirborneMovement();
+        }
+
+        // send input and other state parameters to the animator
+        UpdateAnimator(move);
+    }
+
+    void UpdateAnimator(Vector3 move)
+    {
+        // update the animator parameter
+        m_Animator.SetBool("Grounded", Grounded);
+        if (!Grounded)
+        {
+            m_Animator.SetBool("Grounded", false);
+        }
+
+        if (Grounded)
+        {
+            m_Animator.SetBool("Grounded", true);
+        }
+
+        // the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
+        // which affects the movement speed because of the root motion.
+        if (Grounded && move.magnitude > 0)
+        {
+            m_Animator.speed = m_AnimSpeedMultiplier;
+        }
+        else
+        {
+            // don't use that while airborne
+            m_Animator.speed = 1;
+        }
+    }
+
+    void HandleAirborneMovement()
+    {
+        // apply extra gravity from multiplier:
+        Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
+        m_Rigidbody.AddForce(extraGravityForce);
+
+        m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
+    }
+
+    void HandleGroundedMovement(bool jump)
+    {
+        Debug.Log(jump);
+        // check whether conditions are right to allow a jump:
+        if (jump && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
+        {
+            // jump!
+            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
+            Grounded = false;
+            m_Animator.applyRootMotion = false;
+            m_GroundCheckDistance = 0.1f;
+        }
+        if (!jump)
+        {
+            Vector3 forward = Camera.main.transform.forward;
+            forward.y = 0;
+            forward.Normalize();
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            forward *= vertical;
+            Vector3 right = Camera.main.transform.right;
+            right *= horizontal;
+            Vector3 combined = forward + right;
+            combined.y = 0;
+            combined.Normalize();
+            m_Rigidbody.velocity = combined * 10;
+            if(horizontal == 0 && vertical == 0)
+            {
+                m_Animator.SetBool("Idle", true);
+            }else
+            {
+                m_Animator.SetBool("Idle", false);
+            }
+            //m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
+            Grounded = true;
+            
+        }
+    }
+
+    public void OnAnimatorMove()
+    {
+        // we implement this function to override the default root motion.
+        // this allows us to modify the positional speed before it's applied.
+        if (Grounded && Time.deltaTime > 0)
+        {
+            Vector3 v = (m_Animator.deltaPosition * m_MoveSpeedMultiplier) / Time.deltaTime;
+
+            // we preserve the existing y part of the current velocity.
+            v.y = m_Rigidbody.velocity.y;
+            m_Rigidbody.velocity = v;
+        }
+    }
+
+
+    void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+#if UNITY_EDITOR
+        // helper to visualise the ground check ray in the scene view
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
+#endif
+        // 0.1f is a small offset to start the ray from inside the character
+        // it is also good to note that the transform position in the sample assets is at the base of the character
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
+        {
+            m_GroundNormal = hitInfo.normal;
+            Grounded = true;
+            m_Animator.applyRootMotion = true;
+        }
+        else
+        {
+            Grounded = false;
+            m_GroundNormal = Vector3.up;
+            m_Animator.applyRootMotion = false;
+        }
+    }
+
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
