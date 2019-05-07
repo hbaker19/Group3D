017fb5aa1c831ce160bce34fb4fccd8a55d4aef4
Colby Johnson <20.colby.johnson@ksd.org>
2019-05-07 09:42:52 -0700

    CharacterSwtich(WIP)

---
+++ Assets/Handmade stuff/Characters/Hero(Colby)/CharacterSwtich.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class CharacterSwtich : MonoBehaviour
+{
+
+    public GameObject Colby;
+    public GameObject Robo;
+
+    public GameObject ColbyCam;
+    public GameObject RoboCam;
+
+    public Image ColbyImage;
+    public Image RoboImage;
+
+    public Canvas UI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Colby = GameObject.Find("Colby (Player)");
+        Robo = GameObject.Find("Robo (Player)");
+
+        ColbyCam = GameObject.Find("Camera.C");
+        RoboCam = GameObject.Find("Camera.R");
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3") && RoboImage == false)
+        {
+
+            Colby.GetComponent<Rigidbody>().isKinematic = true;
+            Robo.GetComponent<Rigidbody>().isKinematic = false;
+
+            Colby.GetComponent<ThirdPersonCharacter>().enabled = false;
+            Colby.GetComponent<ThirdPersonUserControl>().enabled = false;
+
+            Robo.GetComponent<ThirdPersonCharacter>().enabled = true;
+            Robo.GetComponent<ThirdPersonUserControl>().enabled = true;
+
+            ColbyCam.GetComponent<Camera>().enabled = false;
+            RoboCam.GetComponent<Camera>().enabled = true;
+
+            ColbyImage.GetComponent<Image>().enabled = true;
+
+        }
+    }
+}
