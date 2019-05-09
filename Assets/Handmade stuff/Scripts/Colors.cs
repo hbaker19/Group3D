ebd5c367c17d153adf65b01540a7380786906c11
Colby Johnson <20.colby.johnson@ksd.org>
2019-05-09 08:44:57 -0700

    SkyBox

---
+++ Assets/Handmade stuff/Scripts/Colors.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Colors : MonoBehaviour
+{
+    public Material ColbyNormal;
+    public Material ColbyAresen;
+    public Material ColbyHannah;
+    public Material ColbyRex;
+
+
+    public Material RoboNormal;
+    public Material RoboGoldnWhite;
+    public Material RoboPinknPurple;
+    public Material RoboBluenOrange;
+
+    public GameObject Colby;
+    public GameObject Robo;
+
+    public RawImage CheckMarkColbyN;
+    public RawImage CheckMarkColbyA;
+    public RawImage CheckMarkColbyH;
+    public RawImage CheckMarkColbyR;
+
+    public RawImage CheckMarkRoboN;
+    public RawImage CheckMarkRoboGnW;
+    public RawImage CheckMarkRoboPinPu;
+    public RawImage CheckMarkRoboBnO;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ColbyN()
+    {
+        Colby.GetComponent<SkinnedMeshRenderer>().material = ColbyNormal;
+        CheckMarkColbyN.GetComponent<RawImage>().enabled = true;
+        CheckMarkColbyA.GetComponent<RawImage>().enabled = false;
+        CheckMarkColbyH.GetComponent<RawImage>().enabled = false;
+        CheckMarkColbyR.GetComponent<RawImage>().enabled = false;
+    }
+
+    public void ColbyA()
+    {
+        Colby.GetComponent<SkinnedMeshRenderer>().material = ColbyAresen;
+        CheckMarkColbyN.GetComponent<RawImage>().enabled = false;
+        CheckMarkColbyA.GetComponent<RawImage>().enabled = true;
+        CheckMarkColbyH.GetComponent<RawImage>().enabled = false;
+        CheckMarkColbyR.GetComponent<RawImage>().enabled = false;
+    }
+    
+    public void ColbyH()
+    {
+        CheckMarkColbyN.GetComponent<RawImage>().enabled = false;
+        CheckMarkColbyA.GetComponent<RawImage>().enabled = false;
+        CheckMarkColbyH.GetComponent<RawImage>().enabled = true;
+        CheckMarkColbyR.GetComponent<RawImage>().enabled = false;
+    }
+
+    public void ColbyR()
+    {
+        CheckMarkColbyN.GetComponent<RawImage>().enabled = false;
+        CheckMarkColbyA.GetComponent<RawImage>().enabled = false;
+        CheckMarkColbyH.GetComponent<RawImage>().enabled = false;
+        CheckMarkColbyR.GetComponent<RawImage>().enabled = true;
+    }
+
+    public void RoboN()
+    {
+        Robo.GetComponent<SkinnedMeshRenderer>().material = RoboNormal;
+        CheckMarkRoboN.GetComponent<RawImage>().enabled = true;
+        CheckMarkRoboGnW.GetComponent<RawImage>().enabled = false;
+        CheckMarkRoboPinPu.GetComponent<RawImage>().enabled = false;
+        CheckMarkRoboBnO.GetComponent<RawImage>().enabled = false;
+    }
+
+    public void RoboGnW()
+    {
+        Robo.GetComponent<SkinnedMeshRenderer>().material = RoboGoldnWhite;
+        CheckMarkRoboN.GetComponent<RawImage>().enabled = false;
+        CheckMarkRoboGnW.GetComponent<RawImage>().enabled = true;
+        CheckMarkRoboPinPu.GetComponent<RawImage>().enabled = false;
+        CheckMarkRoboBnO.GetComponent<RawImage>().enabled = false;
+    }
+
+    public void RoboPinPu()
+    {
+        CheckMarkRoboN.GetComponent<RawImage>().enabled = false;
+        CheckMarkRoboGnW.GetComponent<RawImage>().enabled = false;
+        CheckMarkRoboPinPu.GetComponent<RawImage>().enabled = true;
+        CheckMarkRoboBnO.GetComponent<RawImage>().enabled = false;
+
+    }
+
+    public void RoboBnO()
+    {
+        CheckMarkRoboN.GetComponent<RawImage>().enabled = false;
+        CheckMarkRoboGnW.GetComponent<RawImage>().enabled = false;
+        CheckMarkRoboPinPu.GetComponent<RawImage>().enabled = false;
+        CheckMarkRoboBnO.GetComponent<RawImage>().enabled = true;
+    }
+
+}
---
+++ Assets/Handmade stuff/Scripts/Pause.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+
+    public Canvas PauseMenu;
+    public Canvas UI;
+    public Canvas Customize3;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            //If it is pressed, stop stuff form moving and make the pause menu visible to the naked eye.
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                PauseMenu.GetComponent<Canvas>().enabled = true;
+                UI.GetComponent<Canvas>().enabled = false;
+                Cursor.visible = false;
+                Screen.lockCursor = false;
+            }
+            else
+            {
+                //unpause
+                Resume();
+            }
+
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        PauseMenu.GetComponent<Canvas>().enabled = false;
+        UI.GetComponent<Canvas>().enabled = true;
+    }
+
+    public void Customize()
+    {
+        PauseMenu.GetComponent<Canvas>().enabled = false;
+        Customize3.GetComponent<Canvas>().enabled = true;
+
+    }
+}
