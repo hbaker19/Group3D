9d33a2a1244eb00289a26e026e7ac493ee146971
Colby Johnson <20.colby.johnson@ksd.org>
2019-05-10 10:27:55 -0700

    GabbyChat

---
+++ Assets/Handmade stuff/PreFabs/GabChat.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GabChat : MonoBehaviour
+{
+    public Canvas GabyChatH;
+    public Canvas GabyChatRo;
+    public Canvas GabyChatC;
+    public Canvas GabyChatR;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Hannah")
+        {
+            GabyChatH.GetComponent<Canvas>().enabled = true;
+        }
+        else
+        {
+            GabyChatH.GetComponent<Canvas>().enabled = false;
+        }
+
+        
+        if (other.gameObject.tag == "Rex")
+        {
+
+        }
+
+        if (other.gameObject.tag == "Robo")
+        {
+            GabyChatRo.GetComponent<Canvas>().enabled = true;
+        }else
+        {
+            GabyChatRo.GetComponent<Canvas>().enabled = false;
+        }
+
+
+
+
+  
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Hannah")
+        {
+            GabyChatH.GetComponent<Canvas>().enabled = false;
+        }
+
+        if (other.gameObject.tag == "Robo")
+        {
+            GabyChatRo.GetComponent<Canvas>().enabled = false;
+        }
+        
+    }
+
+}
