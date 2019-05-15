ea5a8aa14296ddaca70ad5ff972f04e2a147c789
Hannah Baker <19.hannah.baker@ksd.org>
2019-05-15 07:51:52 -0700

    Enemy NavMesh animations

--- Assets/Handmade stuff/Enemy Animations/EnemyNavMove.cs
+++ Assets/Handmade stuff/Enemy Animations/EnemyNavMove.cs
@@ -21,19 +21,56 @@ public class EnemyNavMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //move the object towards the destination, which in this case
-        //is the player
+        //Move the object towards the destination, which in this case is the player
 		Vector3 direction = player.transform.position - transform.position;
+
+        //If the player is within range,
         if(direction.magnitude <= chaseDistance){
+            //Start chasing.
 			agent.destination = player.transform.position;
-            GetComponent<Animator>().SetBool("Standing Still", false);
             GetComponent<Animator>().SetBool("Looking Around", false);
             GetComponent<Animator>().SetBool("Start Chasing", true);
+            GetComponent<Animator>().SetBool("Chasing", true);
+            GetComponent<Animator>().SetBool("Going Home", false);
         }
-        else{
-			agent.destination = home;
+        else if ((transform.position - home).magnitude >= 1){
+            //Otherwise, go home.
+            agent.destination = home;
             GetComponent<Animator>().SetBool("Start Chasing", false);
-            GetComponent<Animator>().SetBool("Start Pacing", true);
+            GetComponent<Animator>().SetBool("Chasing", false);
+            GetComponent<Animator>().SetBool("Going Home", true);
         }
-	}
+        else
+        {
+            
+            GetComponent<Animator>().SetBool("Looking Around", true);
+        }
+        Debug.Log(transform.position + " " + home);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //If the enemy walks into a trigger on the player,
+        if (other.gameObject.tag == "Player")
+        {
+            //Start attacking.
+            GetComponent<Animator>().SetBool("Start Attacking", true);
+            GetComponent<Animator>().SetBool("Attacking", true);
+            GetComponent<Animator>().SetBool("Start Chasing", false);
+            GetComponent<Animator>().SetBool("Chasing", false);
+        }   
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //If player is out of range,
+        if (other.gameObject.tag == "Player")
+        {
+            //Resume chasing.
+            GetComponent<Animator>().SetBool("Start Attacking", false);
+            GetComponent<Animator>().SetBool("Attacking", false);
+            GetComponent<Animator>().SetBool("Start Chasing", true);
+            GetComponent<Animator>().SetBool("Chasing", true);
+        }
+    }
 }
